6323176a01c315055fb953999670925d79704f51
NL <natalie.lockhart5@gmail.com>
2019-06-08 15:34:27 -0600

    Adding components
    Adding about-page and dog-page components

--- personal-website/src/app/app.module.ts
+++ personal-website/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResumePageComponent } from './resume-page/resume-page.component';
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { DogPageComponent } from './dog-page/dog-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ResumePageComponent,
-    ProjectsPageComponent
+    ProjectsPageComponent,
+    AboutPageComponent,
+    DogPageComponent
   ],
   imports: [
     BrowserModule,
