aef73a3000c1495b4269b9efdd4ca317ad943957
NL <natalie.lockhart5@gmail.com>
2019-06-23 23:08:56 -0600

    Adding npm packages, dog pictures
    Adding npm package to display images in correct orientation; adding more
    dog pictures

--- personal-website/src/app/dog-page/dog-page.component.ts
+++ personal-website/src/app/dog-page/dog-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 import { DOGPICTURES } from '../dogPictures';
 import { DogPicture } from '../dog-picture';
 
@@ -11,8 +11,10 @@ export class DogPageComponent implements OnInit {
 
   dogPictures : DogPicture[] = [];
   dogPictureLinks = DOGPICTURES;
+  loadImage : any = require('blueimp-load-image');
+  dogPicDiv : any;
 
-  constructor() { }
+  constructor(private rd: Renderer2) {}
 
   ngOnInit() {
     this.populateDogPictures();
@@ -27,8 +29,20 @@ export class DogPageComponent implements OnInit {
 
   determineDimensions(){
     this.dogPictures.forEach(dogPicture => {
-      dogPicture.height = 500;
-      dogPicture.width = 700;
+      this.loadImage(
+        dogPicture.imgSrc, 
+        function(img, data) {
+          if(img.type === "error") {
+            console.error("Error loading image sry");
+          } else {
+            document.body.appendChild(img);
+            //document.querySelector('#dogPicDiv').appendChild(img);
+            console.log("Original image width: ", data.originalWidth);
+            console.log("Original image height: ", data.originalHeight);
+          }
+        },
+        {maxWidth: 600}
+      );
     });
   }
 
--- personal-website/src/app/dogPictures.ts
+++ personal-website/src/app/dogPictures.ts
@@ -1,7 +1,12 @@
 
 export const DOGPICTURES: string[] = [
-    "../assets/dogPictures/20190504_082250.jpg",
-    "../assets/dogPictures/20190507_155928.jpg",
-    "../assets/dogPictures/20190508_160126.jpg",
-    "../assets/dogPictures/20190603_181354.jpg"
+    "../assets/dogPictures/20190622_092748.jpg",
+    "../assets/dogPictures/20190612_160401.jpg",
+    "../assets/dogPictures/20190611_160127.jpg",
+    "../assets/dogPictures/20190607_111144.jpg",
+    "../assets/dogPictures/20190603_181354.jpg",
+    "../assets/dogPictures/20190525_114724.jpg",
+    "../assets/dogPictures/20190515_081607.jpg",
+    "../assets/dogPictures/20190513_160613.jpg",
+    "../assets/dogPictures/20190623_162659.jpg"
 ];
